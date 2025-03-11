dd7b818185496975f843cb146817aceff50c6010
Zhaooo <2820821032@qq.com>
2025-03-11 15:53:16 +0800

    1

---
+++ Practical4/triangle.py
@@ -0,0 +1,7 @@
+n=[1,2,3,4,5,6,7,8,9,10]
+sum=0  
+list=[]
+for i in n: #for every i
+    sum+=i  #sum them together
+    list.append(sum) # append them to the blank list
+print(list) #print 10 results
