c143fb1e365415b1830b8303e3bd7e4d11a0f921
Zhaooo <2820821032@qq.com>
2025-03-11 15:30:23 +0800

    1

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,1 +1,16 @@
-11
+#4.1
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+# Because c=90<f=95, so the first way is fatser.
+
+#4.2
+X=True
+Y=False
+W=X and Y
+print("W:",W)
+#  X  |   Y   |  W=X and Y
+# True| False |   False
