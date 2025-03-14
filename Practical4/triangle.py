0de2c76e2a6fb84d534eb82da6565ee10dadafda
Zhaooo <2820821032@qq.com>
2025-03-14 11:01:49 +0800

    1

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -11,11 +11,21 @@
 #    print list
 #end
 
+#Set newlist as a list of integers from 1 to 10  
+#Set sum as 0  
+#Set list as an empty list  
 
-n=[1,2,3,4,5,6,7,8,9,10]
+#For each element i in newlist:
+#    Add i to sum  
+#    Append sum to list  
+
+#Print list  
+
+
+newlist=[x for x in range(1,11)]
 sum=0  
 list=[]
-for i in n:
+for i in newlist:
     sum+=i  
     list.append(sum) 
 print(list) 
