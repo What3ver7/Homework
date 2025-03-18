5e22b96426da27bc1609bbb85509486bfb475e46
Zhaooo <2820821032@qq.com>
2025-03-18 15:31:12 +0800

    1

--- Practical5/size.py
+++ Practical5/size.py
@@ -1,3 +1,13 @@
+ #Define population data for UK countries and Chinese provinces.
+ #Sort the population data.
+ #Define corresponding labels for each region.
+ #Create a figure with two subplots (side by side).
+ #Plot a pie chart for UK countries with labels and percentages.
+ #Set the title for the UK pie chart.
+ #Plot a pie chart for Chinese provinces with labels and percentages.
+ #Set the title for the China pie chart.
+ #Display the charts.import matplotlib.pyplot as plt
+
 import matplotlib.pyplot as plt
 uk_countries=[57.11,3.13,1.91,5.45]
 china_province=[65.77,41.88,45.28,61.27,85.15]
