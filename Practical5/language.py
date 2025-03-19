501ee9b9600760e0bc4449e7c5a41c362e9de572
Zhaooo <2820821032@qq.com>
2025-03-19 15:56:00 +0800

    111

--- Practical5/language.py
+++ Practical5/language.py
@@ -1,3 +1,12 @@
+ #Import `matplotlib.pyplot` as `plt`.  
+ #Create a dictionary of languages and their popularity percentages.  
+ #Extract keys (languages) and convert percentage values to floats.  
+ #Plot a bar chart with `plt.bar()`.  
+ #Set y-axis ticks from 0 to 100 with intervals of 10.  
+ #Add percentage labels above each bar.  
+ #Label x-axis, y-axis, and set the title.  
+ #Display the chart using `plt.show()`.  
+ #ask for user's input
 import matplotlib.pyplot as plt
 dict={"TypeScript":"38.5%",
       "SQL":"51%",
@@ -15,11 +24,8 @@ plt.xlabel("Application")
 plt.ylabel("Percentage")
 plt.title("Programming language popularity")
 plt.show()
- #Import `matplotlib.pyplot` as `plt`.  
- #Create a dictionary of languages and their popularity percentages.  
- #Extract keys (languages) and convert percentage values to floats.  
- #Plot a bar chart with `plt.bar()`.  
- #Set y-axis ticks from 0 to 100 with intervals of 10.  
- #Add percentage labels above each bar.  
- #Label x-axis, y-axis, and set the title.  
- #Display the chart using `plt.show()`.  
+x=input("which language?")
+if x in dict:
+    print(f"the percentage of developers who use this language is {dict[x]}")
+else:
+    print("language not found")
